a400d05602aa579c09a98272f3f763f5bfce15f8
fabianofrank <fabianofrank@protonmail.com>
2022-11-07 11:36:17 -0300

    Rollback to previous working form

--- src/components/PropertyCreate.js
+++ src/components/PropertyCreate.js
@@ -32,15 +32,6 @@ export default function PropertyCreate() {
     <div className="flex justify-center">
       <form className="bg-gray-100 m-5 rounded-md flex flex-col space-y-2 p-5 w-[1080px]" onSubmit={(e) => handleSubmit(e)}>
         <input className="bg-gray-100 h-8" type="text" name="name" id="name" placeholder="Distric" />
-        <input className="bg-gray-100 h-8" type="text" name="city" id="city" placeholder="City" />
-        <input className="bg-gray-100 h-8" type="text" name="description" id="description" placeholder="Description" />
-        <input className="bg-gray-100 h-8" type="text" name="address" id="address" placeholder="Street, Number" />
-        <input className="bg-gray-100 h-8" type="number" name="bed" id="bed" placeholder="Bedrooms" />
-        <input className="bg-gray-100 h-8" type="number" name="bath" id="bath" placeholder="Bathrooms" />
-        <input className="bg-gray-100 h-8" type="number" name="parking" id="parking" placeholder="Parking slots" />
-        <input className="bg-gray-100 h-8" type="number" step="0.1" name="size" id="size" placeholder="Size in sqm" />
-        <input className="bg-gray-100 h-8" type="number" step="0.1" name="price" id="price" placeholder="Price" />
-        <input className="bg-gray-100 h-8 " type="file" multiple name="images" id="images" />
         <button className="self-center p-3 bg-blue-300 text-gray-100 font-semibold w-36 rounded-full m-5" type="submit">Upload</button>
       </form>
     </div>
