58fed6b5938ad606728f9f0bfa96fb2720e9d041
fabianofrank <fabianofrank@protonmail.com>
2022-11-07 11:43:01 -0300

    Add complete form

--- src/components/PropertyCreate.js
+++ src/components/PropertyCreate.js
@@ -13,10 +13,19 @@ export default function PropertyCreate() {
     const data = new FormData();
 
     data.append('property[name]', event.target.name.value);
+    data.append('property[city]', event.target.city.value);
+    data.append('property[description]', event.target.description.value);
+    data.append('property[address]', event.target.address.value);
+    data.append('property[bed]', event.target.bed.value);
+    data.append('property[bath]', event.target.bath.value);
+    data.append('property[parking]', event.target.parking.value);
+    data.append('property[size]', event.target.size.value);
+    data.append('property[price]', event.target.price.value);
 
     Array.from(event.target.images.files).forEach((image) => {
       data.append('property[images][]', image);
     });
+
     dispatch(addProperty(data))
       .then(() => {
         navigate('/');
@@ -32,6 +41,15 @@ export default function PropertyCreate() {
     <div className="flex justify-center">
       <form className="bg-gray-100 m-5 rounded-md flex flex-col space-y-2 p-5 w-[1080px]" onSubmit={(e) => handleSubmit(e)}>
         <input className="bg-gray-100 h-8" type="text" name="name" id="name" placeholder="Distric" />
+        <input className="bg-gray-100 h-8" type="text" name="city" id="city" placeholder="City" />
+        <input className="bg-gray-100 h-8" type="text" name="description" id="description" placeholder="Description" />
+        <input className="bg-gray-100 h-8" type="text" name="address" id="address" placeholder="Street, Number" />
+        <input className="bg-gray-100 h-8" type="number" name="bed" id="bed" placeholder="Bedrooms" />
+        <input className="bg-gray-100 h-8" type="number" name="bath" id="bath" placeholder="Bathrooms" />
+        <input className="bg-gray-100 h-8" type="number" name="parking" id="parking" placeholder="Parking slots" />
+        <input className="bg-gray-100 h-8" type="number" step="0.1" name="size" id="size" placeholder="Size in sqm" />
+        <input className="bg-gray-100 h-8" type="number" step="0.1" name="price" id="price" placeholder="Price" />
+        <input className="bg-gray-100 h-8 " type="file" multiple name="images" id="images" />
         <button className="self-center p-3 bg-blue-300 text-gray-100 font-semibold w-36 rounded-full m-5" type="submit">Upload</button>
       </form>
     </div>
