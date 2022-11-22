59a5c9cb3a142b8b8a2ec741b6384d6a907e4b56
fabianofrank <fabianofrank@protonmail.com>
2022-11-22 14:10:33 -0300

    Limit from 3 to 5 images per upload

--- src/components/PropertyCreate.js
+++ src/components/PropertyCreate.js
@@ -8,7 +8,7 @@ export default function PropertyCreate() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
+  const uploadForm = (event) => {
     event.preventDefault();
     const data = new FormData();
 
@@ -32,6 +32,25 @@ export default function PropertyCreate() {
       });
   };
 
+  const MIN_LENGTH = 3;
+  const MAX_LENGTH = 5;
+
+  const multipleFilesAlert = (e) => {
+    if (Array.from(e.target.files).length < MIN_LENGTH
+    || Array.from(e.target.files).length > MAX_LENGTH) {
+      e.preventDefault();
+      alert('Please select only 3 to 5 images');
+    }
+  };
+
+  const handleSubmit = (e) => {
+    if (multipleFilesAlert === true) {
+      e.preventDefault();
+    } else {
+      uploadForm(e);
+    }
+  };
+
   if (properties.length > 50) {
     return (
       <h2 className="flex justify-center items-center text-4xl w-full h-full bg-gray-100 absolute">You need to delete a property to add another one</h2>
@@ -49,7 +68,7 @@ export default function PropertyCreate() {
         <input className="bg-gray-100 h-8" type="number" name="parking" id="parking" placeholder="Parking slots" />
         <input className="bg-gray-100 h-8" type="number" step="0.1" name="size" id="size" placeholder="Size in sqm" />
         <input className="bg-gray-100 h-8" type="number" step="0.1" name="price" id="price" placeholder="Price" />
-        <input className="bg-gray-100 h-8 " type="file" multiple name="images" id="images" />
+        <input className="bg-gray-100 h-8" type="file" multiple name="images" id="images" placeholder="select 3 to 5 images" onChange={multipleFilesAlert} />
         <button className="self-center p-3 bg-blue-300 text-gray-100 font-semibold w-36 rounded-full m-5" type="submit">Upload</button>
       </form>
     </div>
